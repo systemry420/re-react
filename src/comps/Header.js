e5214944233c3d1cc5418eca1eb8333ae9e55a06
Kassem <systemry420@gmail.com>
2022-01-02 19:54:08 +0200

    initial commit

---
+++ src/comps/Header.js
@@ -0,0 +1,14 @@
+const Header = (props) => {  //({ title })
+    const onClick = () => {
+        alert('hello')
+    }
+
+    return (
+        <div className="header">
+            <h1 style={{ color: 'red' }}>{props.title}</h1>
+            <button onClick={onClick} className="btn">Add</button>
+        </div>
+    )
+}
+
+export default Header
